50890fb4a9a77e0ae7b7f866f0bb27cf2b3cf05a
Ben Carney <ben@carnivorous.org>
2013-11-26 21:50:56 -0800

    Finishes character animations

--- src/components.js
+++ src/components.js
@@ -2,28 +2,41 @@ Crafty.c('PlayerCharacter', {
   init: function() {
     this.requires('Canvas, Actor, Fourway, Collision, spr_player, SpriteAnimation')
     .fourway(4)
-    .animate('PlayerMovingRight', 0, 0, 1)
-    .animate('PlayerMovingLeft', 0, 0, 1)
+    .animate('PlayerMovingRight', 7, 0, 8)
+    .animate('PlayerMovingLeft', 7, 0, 8)
+    .animate('PlayerMovingDown', 1, 0, 2)
+    .animate('PlayerMovingUp', 4, 0, 5)
 
     var animation_speed = 8;
     this.bind('NewDirection', function(data) {
       if (data.x > 0) {
         this.animate('PlayerMovingRight', animation_speed, -1).unflip();
+        stopX = 6;
       } else if (data.x < 0) {
         this.animate('PlayerMovingLeft', animation_speed, -1).flip();
+        stopX = 6;
       } else if (data.y > 0) {
         this.animate('PlayerMovingDown', animation_speed, -1);
+        stopX = 0;
       } else if (data.y < 0) {
         this.animate('PlayerMovingUp', animation_speed, -1);
+        stopX = 3;
       } else {
         this.stop();
+        this.sprite(stopX, 0);
       }
     });
   },
 });
 
+Crafty.c('Inspectable', {
+  init: function() {
+    this.requires('Canvas, Actor, Tint')
+  }
+});
+
 Crafty.c('Painting', {
   init: function() {
-    this.requires('Canvas, Actor, Collision, spr_painting')
+    this.requires('Canvas, 2D, Inspectable, spr_painting')
   }
 });
--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@ Game = {
   start: function() {
     // Start crafty and set a background color so that we can see it's working
     Crafty.init(600, 200);
-    Crafty.background('url(assets/mountain-rise.png)');
+    Crafty.background('#506a4b');
 
     // Simply start the "Loading" scene to get things going
     Crafty.scene('Loading');
